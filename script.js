ecec7bd54ade35c3f62c383cfc823b8d01a805b7
Tucker Steil <tuckersteil@gmail.com>
2022-03-08 17:58:35 -0500

    first updated

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Tuckersteil@gmail.com";
 document.querySelector("body").appendChild(h2);
+
+const h3 = document.createElement("h3");
+h3.textContent = "Cell: (310)880-9914"
+document.querySelector("body").appendChild(h3);
